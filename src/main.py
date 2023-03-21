c2e5dddc80248d9c3112f1cb7f014b543cd836b3
GenyaElamkov <GenyaElamkov@gmail.com>
2023-03-21 18:50:35 +0300

    [upd] insert func set_except_extensions update func copy_file_in_dir

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ from datetime import datetime
 from PIL import ExifTags
 from PIL import Image
 
+
 def create_directory(dir_name: str) -> None:
     """
     Создаём директорию.
@@ -21,6 +22,23 @@ def create_directory(dir_name: str) -> None:
     dir_true = os.path.isdir(dir_name)
     if not dir_true:
         os.mkdir(dir_name)
+
+
+def get_extensions(file_name: str) -> str:
+    """
+    Получаем расширения файлов.
+    """
+    return file_name.split('.')[-1].lower()
+
+
+def set_except_extensions(file_name) -> bool:
+    """
+    Устанавливаем исключения для расширений.
+    Расширения в этом списке не копируются.
+    """
+    excep = ['exe', 'psd']
+    return get_extensions(file_name) in excep
+
 
 def copy_file_in_dir(start_dir: str, finish_dir: str) -> None:
     """
@@ -35,7 +53,9 @@ def copy_file_in_dir(start_dir: str, finish_dir: str) -> None:
                 new_name = str(len(os.listdir(finish_dir))) + file
                 copy_path = os.path.join(finish_dir, new_name)
 
-            shutil.copy2(path, copy_path)
+            # Проверяем на расширения из списка исключений.
+            if not set_except_extensions(file):
+                shutil.copy2(path, copy_path)
 
 
 def get_dates(file_name: str) -> datetime:
@@ -59,35 +79,17 @@ def get_metadates(file_name: str) -> str | None:
     return exif.get('DateTime')
 
 
-def get_extensions(file_name: str) -> str:
-    """
-    Получаем расширения файлов.
-    """
-    return file_name.split('.')[-1].lower()
-
-
 def _checking_metadata(file_name: str) -> bool:
     """
     Проверка файла на метаданные.
+    list_extensions - список расширений для метаданных.
     """
     list_extensions = ['jpeg', 'png', 'jpg']
     return get_extensions(file_name=file_name) in list_extensions \
            and get_metadates(file_name=file_name) is not None
 
 
-def main():
-    start_path = r"C:\Users\User\Desktop\фото_Еська\Катюша"
-    # start_path = input("Скопируйте сюда путь к фото: ")
-    # Текущая директория.
-    # start_path = os.getcwd()
-    os.chdir(start_path)
-
-    # Директория куда складываем файлы.
-    finish_dir = 'NewFolder'
-    create_directory(finish_dir)
-
-    # Путь куда складываем файлы.
-    finish_path = os.path.join(start_path, finish_dir)
+def work(start_path: str, finish_path: str) -> None:
     # Копируем файлы.
     copy_file_in_dir(start_dir=start_path, finish_dir=finish_path)
 
@@ -98,6 +100,7 @@ def main():
     for root, dirs, files in os.walk("."):
         for file in files:
             path = os.path.join(root, file)
+
             # Получаем метаданные с фото.
             metadates_true = _checking_metadata(file_name=path)
             if metadates_true:
@@ -115,10 +118,26 @@ def main():
             create_directory(month)
             os.chdir('..')
 
+            # Перемещаем копированные файлы в созданные директорий.
             try:
                 shutil.move(file, os.path.join(year, month, file))
             except FileNotFoundError:
                 print('[!] Файл перенесен')
+
+
+def main() -> None:
+    # Текущая директория.
+    start_path = r"C:\Users\User\Desktop\фото_Еська\Катюша"
+    # start_path = input("Скопируйте сюда путь к фото: ")
+    # start_path = os.getcwd()
+    os.chdir(start_path)
+
+    # Директория куда складываем файлы.
+    finish_dir = 'NewFolder'
+    create_directory(finish_dir)
+    finish_path = os.path.join(start_path, finish_dir)
+
+    work(start_path, finish_path)
 
 
 if __name__ == '__main__':
